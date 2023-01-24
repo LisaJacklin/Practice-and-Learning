ba2e346037d30b6b5e7ce2c93f9b167fad506f0b
LisaJacklin <lmjacklin@alaska.edu>
2023-01-23 19:25:48 -0900

    lab0Prep

---
+++ C-C++/CppPractice/Source1.cpp
@@ -0,0 +1,41 @@
+//Lisa Jacklin
+// 1/23/2023
+// Cpp Practice code
+
+/*
+Code that should be practiced for EE444, EE443, and probably even CS321. 
+All code in here comes primarily from CS 201/202 as well as aditional code 
+that I have created in these classes and others.
+
+This practice set is intended to be used as a reference for later code development 
+in order to allow me to easily practice, and adjust code that will later be developed.
+
+*/
+
+//standard library as well as the shortcuts to avoid using the std:: key
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::vector;
+
+int x; //global variable
+
+//this is a function built to take two input integers 
+foo(int a, int b) {}; 
+
+//this is a vector that takes strings named args.
+vector <std::string> args;
+
+//this is a class of students.
+class Students {
+public: // items within public can be accessed by anyone
+private: // items in private can only be accessed by this class.
+};
+
+//a program is not complete without having a main.
+ main() {
+
+};
