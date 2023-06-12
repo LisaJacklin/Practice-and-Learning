42a1ae5603f992f995509a46dc4644b93310ec5e
Lisa Jacklin <lmjacklin@alaska.edu>
2023-06-12 14:52:02 -0800

    Create beginning.py

---
+++ Python/beginning.py
@@ -0,0 +1,14 @@
+"""
+PYTHON STUDY NOTES
+
+if you use three " in a row, you can do multi line comments
+"""
+#just like in ARM64, a pound is used for single line commands
+#the appropriate ending for a python script is .py as well
+
+#to display something, print is used
+#note the minimal syntax for python. no end syntax for lines.
+print("hello")
+
+#bool, and most mathematical operations are done the exact same as in C/Cpp
+
