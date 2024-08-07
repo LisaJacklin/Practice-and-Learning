7b3c3a3ae2428003fd3ddc590c1d782277acf79a
LisaJacklin <lmjacklin@alaska.edu>
2024-08-07 11:46:52 -0800

    examples work: basic vars, etc...

---
+++ Ruby/ConFlow.rb
@@ -0,0 +1,20 @@
+## ruby essentials: control flow and collections
+
+#large comments use =begin/=end to state where the comments are.
+#=begin
+	this is a collection of basic info and tests for running ruby.
+	I intend on walking through and providing examples of some of the baseline items
+	here as later reference.
+
+	Lisa Jacklin 8/24
+#=end
+
+#creating variables is just like it is any other time in almost any other language
+my_var = 5
+string = "Hello World"
+
+#note that in order for these to be printed, puts is the key word
+puts my_var
+puts string
+
+
