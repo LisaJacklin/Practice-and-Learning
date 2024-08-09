6413df68378f78cb9b3a891531bfee08048ed17e
Lisa Jacklin <lmjacklin@alaska.edu>
2024-08-08 16:03:35 -0800

    finalized notes on http setups

--- Ruby/IO.rb
+++ Ruby/IO.rb
@@ -56,4 +56,47 @@ puts http_response.code
 #here it should provide the value 200. This means its successful!
 
 
+#Now onto parsing Http response
 
+puts http_response.body
+
+#this response wouldn't make any human sense...so theres ore that needs to occur!
+
+require ' net/http'
+require 'uri'
+require 'json'
+
+uri = URL('http://www.example.com/sample.json')
+response = Net::HTTP.get(uri)
+json_response = JSON(response)
+
+#note that uri stands for:Uniform resource Identifiers
+
+#Receiving HTTP requests
+#in order to begin doing this, you have to be able to listen for requests
+# on the network port. (53...?)
+
+# there are various ways to rwite a http server through the ruby toolbox, but for
+#the example, WEBrick is used.
+
+require 'webrick'
+
+#now to specify the port to listen to HTTP requests
+http_server = WEBrick:HTTPServer.new(:Port => 3000)
+
+#how this server has to inherty some functionalities
+class MyHTTPServlet < WEBrick::HTTPServlet::AbstractServlet
+	#this will output the requested path
+	def do_GET(http_request, http_response)
+		http_response.body = "You requested ' #{http_request.path}'"
+	end
+end
+
+http_server.mount {'', MyHTTPServlet}
+#now to stop th eserver use control C with the followig command
+trap ('INT') {http_server.shutdown}
+http_server.start
+
+#with the server running you can make some requests:
+curl localhost:3000
+#this requests '/'
